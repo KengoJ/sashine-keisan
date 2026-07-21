f1e3311650f3c9ec4111ee76385ed497d8e2551d
Kengo.M <gorira0317@gmail.com>
2026-07-21 16:28:18 +0900

    second

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/sashine-keisan/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
